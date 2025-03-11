b7157c47ff4052810224f1c2ad2c2031c7cb10fb
iP4uL0 <paulobapsantos@gmail.com>
2025-03-11 11:59:06 -0300

    11/03

---
+++ app/index.jsx
@@ -0,0 +1,87 @@
+// puxando da bilbioteca 
+import { View, Text,  TextInput, Pressable,  StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome"
+import styled from "styled-components/native"
+// 
+export default function App(){
+    return(
+    <Tela>
+        <Titulo >Entrar</Titulo>
+        <Container >
+            <CamposTexto 
+                placeholder="Digite seu e-mail..." 
+                placeholderTextColor={'#6C757D'}
+               
+            />
+            <CamposTexto 
+                secureTextEntry={true}
+                placeholder="Digite sua senha..." 
+                placeholderTextColor={'#6C757D'}
+               
+            />
+        </Container>
+        <ContainerBotao>
+            <Botao>
+                <TextoBotao>Entrar</TextoBotao>
+            </Botao>
+            <LinhaIcones>
+                <Icon name="google" size={30} color="#fff" ></Icon>
+                <Icon name="instagram" size={30} color="#fff" ></Icon>
+                <Icon name="github" size={30} color="#fff" ></Icon>
+            </LinhaIcones>
+            <Links >Cadastre-se</Links>
+            <Links >Esqueci a senha</Links>
+        </ContainerBotao>
+    </Tela>)
+}
+
+const Tela = styled.View`
+flex: 1;
+background-color:#151932 ;
+padding: 26px;
+
+`
+const Titulo = styled.Text`
+font-size: 56px;
+font-weight: bold;
+color: #ffff;
+margin-top: 80px;
+margin-bottom: 80px;
+`
+const Container = styled.View`
+gap: 25;
+`
+const CamposTexto = styled.TextInput`
+background-color: #fff;
+font-size: 18px;
+text-align: center;
+padding:20px;
+border-radius: 20px;
+`
+const ContainerBotao = styled.View`
+margin-top: 65px;
+gap: 25;
+`
+const Botao = styled.Pressable`
+background-color: #fff;
+padding: 20px;
+border-radius: 20px;
+margin-top: 35px;
+`
+const TextoBotao = styled.Text`
+text-align: center;
+font-size: 24px;
+font-weight: bold;
+`
+const Links = styled.Text`
+text-align: center;
+color:#fff;
+font-size: 16px;
+margin-top: 15px;
+`
+const LinhaIcones = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    gap: 15;
+    
+`
